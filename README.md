bca90d52861bd4a90d56e55adf221fc6b4973af1
harsh1234bit <74101952+harsh1234bit@users.noreply.github.com>
2020-12-03 12:02:20 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# project-21-whjrc
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,61 @@
+var wall,thickness;
+var bullet,speed,weight;
+
+
+
+function setup() {
+  createCanvas(1600,400);
+
+speed=random(30,100)
+weight=random(30,52)
+thickness=random(22,83)
+
+bullet = createSprite(50,200,50,5);
+bullet.velocityX= speed;
+bullet.shapeColor=color("black")
+
+wall = createSprite(1200,200,thickness,height/2);
+wall.shapeColor=color(80,80,80)
+ 
+}
+
+function draw() {
+  background(255,255,255);
+ 
+  
+  if(hasCollided(bullet,wall))
+  { 
+  bullet.velocityX=0;
+   var damage=0.5 * weight * speed* speed/(thickness *thickness);
+  
+   if(damage>10)
+  {
+  
+   wall.shapeColor=color(255,0,0);
+  }
+  
+   if(damage<10)
+  {
+  
+   wall.shapeColor=color(0,255,0);
+  }
+}
+  drawSprites();
+}
+
+
+function hasCollided (objbullet, objwall)
+{
+bulletRightEdge=objbullet.x + objbullet.width;
+objwallLeftEdge=objwall.x;
+
+if(bulletRightEdge>=objwallLeftEdge)
+{ return true
+}
+else    {
+return false;
+}
+
+
+
+}
